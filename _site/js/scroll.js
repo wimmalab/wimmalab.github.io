66864ecfd4fbe54799b440ceb91a6abd6bb813fc
Artur Veselovski <artur.veselovski@hotmail.fi>
2017-06-08 17:26:38 +0300

    Final-ish version. Added logos, updated pictures, changed landing and about pages

--- _site/js/scroll.js
+++ _site/js/scroll.js
@@ -1,27 +1,3 @@
-/* $(function() {
-    // Smooth Scrolling
-    $('a[href*="#"]:not([href="#"])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-            if (target.length) {
-                $('html, body').animate({
-                    scrollTop: target.offset().top - 50
-                }, 1000);
-                return false;
-            }
-        }
-    });
-}); */
-
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});
-
 $(function() {
     $(document).on('click', 'a.page-scroll', function(event) {
         var $anchor = $(this);
