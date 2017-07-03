ad2736b4ae5b948b50b2aa85261c95ebf8538a8c
Maiju123 <K2322@student.jamk.fi>
2017-07-03 15:02:10 +0300

    image added

--- src/components/Companies.js
+++ src/components/Companies.js
@@ -1,11 +1,12 @@
 // libs
 import React from 'react';
+import PengWinM from '../img/companies/pengWin_hero.png';
 
 export default class Companies extends React.Component {
     render() {
         return (
             <section className="companies">
-                <div className="container">vCompanies content here</div>
+                <div className="container"><img src={PengWinM} alt={PengWinM} /></div>
             </section>
         );
     }
