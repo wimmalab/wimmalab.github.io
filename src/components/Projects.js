4134b1dc6f798a02be22b324575f1148b0536361
Maiju123 <K2322@student.jamk.fi>
2017-07-14 11:52:26 +0300

    project, information added - need designing still

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,6 +6,15 @@ import Freenest from '../img/projects/whitetext_freenest_logo_vertical_rgb.png';
 import Toolbar from '../img/projects/toolbar2014.png';
 import Unity from '../img/projects/unity2014.png';
 import Fbmt from '../img/projects/fbmt2014.PNG';
+import Jarmo from '../img/projects/jarmo2015.png';
+import Noise from '../img/projects/noise2015.png';
+import Kumos from '../img/projects/kumos2016.png';
+import Overflow from '../img/projects/overflow2016.png';
+import IoTitude from '../img/projects/iotitude2016.png';
+import PengWinGame from '../img/projects/PengWinGame.png';
+import PengWinMedia from '../img/projects/PengWinMedia.png';
+import MystiCons from '../img/projects/mysticons2017.png';
+
 
 export default class Projects extends React.Component {
     render() {
@@ -13,75 +22,151 @@ export default class Projects extends React.Component {
             <div className="projects-content">
                 <h2>WIMMA Lab Projects</h2>
                 <hr />
-                <p>WIMMA Lab has exist from 2012, and here has been done many different projects. </p>
-                <h2>2017</h2>
-                <div className="projects-icons">
-                    <a><div className="projects-icon"><img src={Contriboard} alt="contriboard"/><div className ="hover-projects"><p>Contriboard</p></div></div></a>
-                    <a><div className="projects-icon"><img src={Contriboard} alt="contriboard"/><div className ="hover-projects"><p>Contriboard</p></div></div></a>
-                    <a><div className="projects-icon"><img src={Contriboard} alt="contriboard"/><div className ="hover-projects"><p>Contriboard</p></div></div></a>
-                    <a><div className="projects-icon"><img src={Contriboard} alt="contriboard"/><div className ="hover-projects"><p>Contriboard</p></div></div></a>
-                </div>
-                <h2>2016</h2>
-                <div className="projects-icons">
-                    <a><div className="projects-icon"><img src={Contriboard} alt="contriboard"/><p>Contriboard</p></div></a>
-                    <a><div className="projects-icon"><img src={Contriboard} alt="contriboard"/><p>Contriboard</p></div></a>
-                    <a><div className="projects-icon"><img src={Contriboard} alt="contriboard"/><p>Contriboard</p></div></a>
-                    <a><div className="projects-icon"><img src={Contriboard} alt="contriboard"/><p>Contriboard</p></div></a>
-                </div>
-                <h2>2015</h2>
+                <h3>2017 - WIMMA Lab</h3>
                 <div className="projects-icons">
                     <div className="projects-icon">
-                        <img src={Freenest} alt="Freenest"/>
-                        <a href="http://freenest.org/about">
+                        <img src={Overflow} alt="Overflow"/>
+                        <a href="#">
                             <div className ="hover-projects">
-                                <h5>FreeNest is a team-oriented product development platform that is fast to deploy and easy to use.
+                                <h5>Overflow builds a Software Defined Network to Challenge Factory 2016.
                                 </h5>
                             </div>
                         </a>
                     </div>
                     <div className="projects-icon">
-                        <img src={Freenest} alt="Freenest"/>
-                        <a href="http://freenest.org/about">
+                        <img src={IoTitude} alt="IoTitude"/>
+                        <a href="#">
                             <div className ="hover-projects">
-                                <h5>FreeNest is a team-oriented product development platform that is fast to deploy and easy to use.
+                                <h5> With our service you can maintain and monitor your pipe network and collect accurate data from water flow to water levels and much more!
+                                </h5>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="projects-icon">
+                        <img src={PengWinGame} alt="PengWinGame"/>
+                        <a href="#">
+                            <div className ="hover-projects">
+                                <h5> Wine Game! asdASDFA
+                                </h5>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="projects-icon">
+                        <img src={PengWinMedia} alt="PengWinMedia"/>
+                        <a href="#">
+                            <div className ="hover-projects">
+                                <h5> Media jee sadasd!
+                                </h5>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="projects-icon">
+                        <img src={MystiCons} alt="MystiCons"/>
+                        <a href="#">
+                            <div className ="hover-projects">
+                                <h5>Give dataaaa uhhH!
+                                </h5>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <a href="https://n4sjamk.github.io/challenge-factory-2016.html">
+                    <h3>2016 - Challenge Factory</h3>
+                </a>
+                <div className="projects-icons">
+                    <div className="projects-icon">
+                        <img src={Kumos} alt="Kumos"/>
+                        <a href="http://kumos.github.io/">
+                            <div className ="hover-projects">
+                                <h5>Kumos is all about the Cloud. We aim to provide YOU the best Cloud you have ever had.
+                                </h5>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="projects-icon">
+                        <img src={Overflow} alt="Overflow"/>
+                        <a href="https://overflowjamk.github.io/">
+                            <div className ="hover-projects">
+                                <h5>Overflow builds a Software Defined Network to Challenge Factory 2016.
+                                </h5>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="projects-icon">
+                        <img src={IoTitude} alt="IoTitude"/>
+                        <a href="https://iotitude.github.io/">
+                            <div className ="hover-projects">
+                                <h5> With our service you can maintain and monitor your pipe network and collect accurate data from water flow to water levels and much more!
+                                </h5>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <a href="https://n4sjamk.github.io/challenge-factory-2015.html">
+                    <h3>2015 - Challenge Factory</h3>
+                </a>
+                <div className="projects-icons">
+                    <div className="projects-icon">
+                        <img src={Contriboard} alt="contriboard"/>
+                        <a href="http://n4sjamk.github.io/contriboard/">
+                            <div className ="hover-projects">
+                                <h5>Think about the person writing down the various handwritten tickets! With Contriboard your ideas will be in digital form from the beginnig to the end of the storm session.
+                                </h5>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="projects-icon">
+                        <img src={Jarmo} alt="Jarmo"/>
+                        <a href="https://n4sjamk.github.io/jarmo/">
+                            <div className ="hover-projects">
+                                <h5>Jarmo is a simple system for application health monitoring.
+                                </h5>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="projects-icon">
+                        <img src={Noise} alt="Noise"/>
+                        <a href="https://n4sjamk.github.io/challenge-factory-2015.html">
+                            <div className ="hover-projects">
+                                <h5>Simulate different network conditions
                                 </h5>
                             </div>
                         </a>
                     </div>
                 </div>
             <a href="https://n4sjamk.github.io/challenge-factory-2014.html">
-                <h2>2014</h2>
+                <h3>2014 - Summer Factory </h3>
             </a>
                 <div className="projects-icons">
-                <div className="projects-icon">
-                    <img src={Toolbar} alt="Toolbar"/>
-                    <a href="https://n4sjamk.github.io/challenge-factory-2014.html">
-                        <div className ="hover-projects">
-                            <h5>Collect and share sites and tools your team needs speeding up the working environment setup process.
-                            </h5>
-                        </div>
-                    </a>
+                    <div className="projects-icon">
+                        <img src={Toolbar} alt="Toolbar"/>
+                        <a href="https://n4sjamk.github.io/challenge-factory-2014.html">
+                            <div className ="hover-projects">
+                                <h5>Collect and share sites and tools your team needs speeding up the working environment setup process.
+                                </h5>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="projects-icon">
+                        <img src={Fbmt} alt="Fbmt"/>
+                        <a href="https://n4sjamk.github.io/challenge-factory-2014.html">
+                            <div className ="hover-projects">
+                                <h5>Collect and share sites and tools your team needs speeding up the working environment setup process.
+                                </h5>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="projects-icon">
+                        <img src={Unity} alt="Unity"/>
+                        <a href="https://www.youtube.com/watch?v=jvpoDnsj280&feature=youtu.be">
+                            <div className ="hover-projects">
+                                <h5>Simulation of prototypes in a natural environment using Unity3D as the rendering engine.
+                                </h5>
+                            </div>
+                        </a>
+                    </div>
                 </div>
-                <div className="projects-icon">
-                    <img src={Fbmt} alt="Fbmt"/>
-                    <a href="https://n4sjamk.github.io/challenge-factory-2014.html">
-                        <div className ="hover-projects">
-                            <h5>Collect and share sites and tools your team needs speeding up the working environment setup process.
-                            </h5>
-                        </div>
-                    </a>
-                </div>
-                <div className="projects-icon">
-                    <img src={Unity} alt="Unity"/>
-                    <a href="https://www.youtube.com/watch?v=jvpoDnsj280&feature=youtu.be">
-                        <div className ="hover-projects">
-                            <h5>Simulation of prototypes in a natural environment using Unity3D as the rendering engine.
-                            </h5>
-                        </div>
-                    </a>
-                </div>
-                </div>
-                <h2>2012 & 2013</h2>
+                <h3>2012 & 2013 - Summer Factory</h3>
                 <div className="projects-icons">
                     <div className="projects-icon">
                         <img src={Freenest} alt="Freenest"/>
